8e99f668483eaf2426c18c30284f16ad51400f97
BW <bartoszwais@googlemail.com>
2020-04-17 15:39:11 +0200

    suggested ordering excerpt reduced to 120 first pieces

--- tools/xmas_tree.rb
+++ tools/xmas_tree.rb
@@ -1,4 +1,4 @@
-a = (1..101).to_a.map { |x| x.to_s }
+a = (1..16).to_a.map { |x| x.to_s }
 tree = a.map.with_index { |x, i|  "<span class='no'></span>" * i }.map {|x| "<p>#{x}</p>"}
 
 tree[1..-1].each { |x|
